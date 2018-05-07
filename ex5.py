69fe00db56d019a76c5c62ef0a59c60f0a637c80
sreeky <36283333+kanthian@users.noreply.github.com>
2018-05-07 16:55:56 -0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'srikanth'
+my_age = 28 #lie
+my_height = 69
+my_weight = 150 #lbs
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'colored'
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "That's healthy!"
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "He can date someone who is atleast %d years or older" % (my_age/2+7)
+print "His weight in metric system is %d kgs" %(round(my_weight*0.45))
